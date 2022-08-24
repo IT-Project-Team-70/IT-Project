d6627784b8ff6c73fe7a79f7abecfd6a23d15713
Bowen Fan <bowen.fan@student.unimelb.edu>
2022-08-25 00:36:42 +1000

    Added navbar and recipe card components for Personal Kitchen

--- front-end/src/containers/feature/personalKitchen/index.js
+++ front-end/src/containers/feature/personalKitchen/index.js
@@ -1,7 +1,74 @@
-import React from 'react'
+/* eslint-disable prettier/prettier */
+import * as React from 'react'
+import Box from '@mui/material/Box'
+import Drawer from '@mui/material/Drawer'
+import Toolbar from '@mui/material/Toolbar'
+import List from '@mui/material/List'
+import Divider from '@mui/material/Divider'
+import ListItem from '@mui/material/ListItem'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+import MenuBookIcon from '@mui/icons-material/MenuBook'
+import FastfoodIcon from '@mui/icons-material/Fastfood'
+import AddCircleOutlinedIcon from '@mui/icons-material/AddCircleOutlined'
+import { ThemeProvider } from '@mui/material'
+import useTheme from '../../../css/muiTheme/index'
+import RecipeCard from '../../recipe/recipeCard'
+
+const drawerWidth = 240
 
 const PersonalKitchen = (props) => {
-  return <div>PersonalKitchen</div>
+  const theme = useTheme()
+  return (
+    <ThemeProvider theme={theme}>
+      <Box sx={{ display: 'flex' }}>
+        <Drawer
+          variant="permanent"
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            [`& .MuiDrawer-paper`]: {
+              width: drawerWidth,
+              boxSizing: 'border-box',
+            },
+            zIndex: -1,
+          }}
+        >
+          <Toolbar />
+          <Box sx={{ overflow: 'auto' }}>
+            <List>
+              {['My Recipes', 'Favourites'].map((text, index) => (
+                <ListItem key={text} disablePadding>
+                  <ListItemButton>
+                    <ListItemIcon>
+                      {index % 2 === 0 ? <MenuBookIcon /> : <FastfoodIcon />}
+                    </ListItemIcon>
+                    <ListItemText primary={text} />
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List>
+            <Divider />
+            <List>
+              {['New Recipe'].map((text, index) => (
+                <ListItem key={text} disablePadding>
+                  <ListItemButton>
+                    <ListItemIcon>
+                      <AddCircleOutlinedIcon />
+                    </ListItemIcon>
+                    <ListItemText primary={text} />
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+        </Drawer>
+          <RecipeCard></RecipeCard>
+          <RecipeCard></RecipeCard>
+      </Box>
+    </ThemeProvider>
+  )
 }
 
 PersonalKitchen.propTypes = {}
---
+++ front-end/src/containers/recipe/recipeCard.js
@@ -0,0 +1,57 @@
+import * as React from 'react'
+import Card from '@mui/material/Card'
+import CardHeader from '@mui/material/CardHeader'
+import CardMedia from '@mui/material/CardMedia'
+import CardContent from '@mui/material/CardContent'
+import CardActions from '@mui/material/CardActions'
+import Avatar from '@mui/material/Avatar'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import { red } from '@mui/material/colors'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import ShareIcon from '@mui/icons-material/Share'
+import MoreVertIcon from '@mui/icons-material/MoreVert'
+import placeholderImage from './PlaceholderImage.png'
+
+export default function RecipeCard() {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            A
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="(Author Name)"
+        subheader="January 1, 2022"
+      />
+      <CardMedia
+        component="img"
+        height="200"
+        image={placeholderImage}
+        alt="Food Image"
+      />
+      <CardContent>
+        <Typography variant="body1" color="text.primary">
+          (Food name here)
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          (Short description of the recipe here)
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+      </CardActions>
+    </Card>
+  )
+}
