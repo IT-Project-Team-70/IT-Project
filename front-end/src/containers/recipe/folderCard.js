cfc378b5d50fed0896bc1e734991539a808838ea
Bowen Fan <bowen.fan@student.unimelb.edu>
2022-09-01 15:10:49 +1000

    Added folder component

---
+++ front-end/src/containers/recipe/folderCard.js
@@ -0,0 +1,31 @@
+import * as React from 'react'
+import Card from '@mui/material/Card'
+import CardMedia from '@mui/material/CardMedia'
+import CardContent from '@mui/material/CardContent'
+import Typography from '@mui/material/Typography'
+import { ThemeProvider } from '@mui/material'
+import useTheme from '../../css/muiTheme'
+import placeholderImage from './PlaceholderImage.png'
+
+export default function FolderCard(props) {
+  const theme = useTheme()
+  return (
+    <ThemeProvider theme={theme}>
+      <Card sx={{ maxWidth: 380, bgcolor: 'primary.main' }}>
+        <CardMedia
+          component="img"
+          height="350"
+          image={placeholderImage}
+          alt="Folder Image"
+        />
+        <CardContent>
+          <Typography variant="h4" color="white">
+            (Folder Name Here)
+          </Typography>
+        </CardContent>
+      </Card>
+    </ThemeProvider>
+  )
+}
+
+FolderCard.propTypes = {}
