958948bf1eb65d7d3f105af05e547af103c81a3b
Yung-Ching Lin <76548593+joycekyery@users.noreply.github.com>
2022-09-27 05:48:29 +1000

    modify view recipe

--- front-end/src/containers/viewRecipe/index.js
+++ front-end/src/containers/viewRecipe/index.js
@@ -300,33 +300,58 @@ const ViewRecipe = ({ match, ...props }) => {
                         <Typography variant="h6" sx={{ paddingBottom: '8px' }}>
                           Category
                         </Typography>
-                        {recipeData && recipeData.courseList && (
+                        {recipeData && recipeData.tagNameList && (
                           <Box>
-                            <Typography
-                              variant="body1"
-                              sx={{ paddingBottom: '8px' }}
-                            >
-                              Course:
-                            </Typography>
-                            {recipeData.courseList.map((course, index) => (
-                              <Chip
-                                key={course}
-                                label={course}
-                                sx={{ marginLeft: '8px' }}
-                              ></Chip>
-                            ))}
-                          </Box>
-                        )}
-                        {recipeData && recipeData.tagList && (
-                          <Box>
-                            <Typography variant="body1">Categories:</Typography>
-                            {recipeData.tagList.map((tag, index) => (
+                            {recipeData.tagNameList.map((tag, index) => (
                               <Chip
                                 key={tag}
                                 label={tag}
                                 sx={{ marginLeft: '8px' }}
                               ></Chip>
                             ))}
+                          </Box>
+                        )}
+                      </Box>
+                      <Box
+                        component={Paper}
+                        p={3}
+                        sx={{
+                          backgroundColor: '#fcfcfc',
+                          marginTop: '8px',
+                          alignSelf: 'center',
+                          width: 'calc(100% - 48px)',
+                        }}
+                      >
+                        <Typography variant="h6" sx={{ paddingBottom: '8px' }}>
+                          Source
+                        </Typography>
+                        {recipeData && recipeData.source && (
+                          <Box>
+                            {recipeData.source.type === 'Own' && (
+                              <Typography
+                                variant="body1"
+                                sx={{ paddingBottom: '8px' }}
+                              >
+                                My own recipe
+                              </Typography>
+                            )}
+                            {recipeData.source.type === 'URL' && (
+                              <a
+                                rel="noopener noreferrer"
+                                href={recipeData.source.content}
+                                target="_blank"
+                              >
+                                {recipeData.source.content}
+                              </a>
+                            )}
+                            {recipeData.source.type === 'Book' && (
+                              <Typography
+                                variant="body1"
+                                sx={{ paddingBottom: '8px' }}
+                              >
+                                {recipeData.source.content}
+                              </Typography>
+                            )}
                           </Box>
                         )}
                       </Box>
@@ -354,6 +379,7 @@ const ViewRecipe = ({ match, ...props }) => {
                           config={{
                             allowResizeX: false,
                             allowResizeY: false,
+                            height: 'auto',
                           }}
                         />
                       )}
