d36360fe635eed65e4dc554a0cd5d74740ed0499
Jerry2108 <jerrykimbum4ever@gmail.com>
2022-09-26 14:56:42 +1000

    push remove recipe

--- back-end/controllers/everyoneKitchenController.js
+++ back-end/controllers/everyoneKitchenController.js
@@ -1,6 +1,7 @@
 const Recipe = require("../models/recipe");
 const User = require("../models/user")
 const recipeHelper = require("../helper/recipeHelper");
+const generalHelper = require("../helper/generalHelper")
 var formidable = require("formidable");
 const { isEmpty } = require("lodash");
 const { all } = require("../routes/personalKitchen");
@@ -46,23 +47,51 @@ async function getOneRecipeById(req, res) {}
 
 async function addFavorite(req, res) {
   try{
+    if(!generalHelper.isValidObjectId(req.params.id)){
+      return res.status(404).send('invalid recipeId')
+    }
     const user = await User.findById(req.user._id)
     const recipe = await Recipe.findById(req.params.id)
     if(!recipe){
       return res.status(200).send('recipe not found')
     }
     const favorites = user.favorites
+    if(!favorites.includes(req.params.id)){
+      favorites.push(recipe)
+    }
     user.favorites = favorites
     await user.save()
     return res.status(200).send(user)
   }
   catch(err){
-    res.status(500).send('Errors while adding recipes')
+    res.status(500).send('Errors while adding favourite recipes')
     throw new Error(err)
   }
 }
 
-async function removeFavorite(req, res) {}
+async function removeFavorite(req, res) {
+  try{
+    if(!generalHelper.isValidObjectId(req.params.id)){
+      return res.status(404).send('invalid recipeId')
+    }
+    const user = await User.findById(req.user._id)
+    const recipe = await Recipe.findById(req.params.id)
+    if(!recipe){
+      return res.status(200).send('recipe not found')
+    }
+    const favorites = user.favorites
+    if(favorites.includes(req.params.id)){
+      favorites.remove(recipe)
+    }
+    user.favorites = favorites
+    await user.save()
+    return res.status(200).send(user)
+  }
+  catch(err){
+    res.status(500).send('Errors while removing favourite recipes')
+    throw new Error(err)
+  }
+}
 
 async function rateRecipe(req, res) {}
 async function commentRecipe(req, res) {}
